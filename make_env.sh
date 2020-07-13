d3b5708f4422d712f7f1decce8062a3d6eb174c6
Tazro Inutano Ohta <inutano@gmail.com>
2020-07-13 14:51:37 +0900

    change home dir

--- make_env.sh
+++ make_env.sh
@@ -7,7 +7,7 @@ set -eu
 JUPYTER_DOCKER_IMAGE=jupyter/datascience-notebook:python-3.8.3
 
 rm -f .env
-cat <<EOT >> $(cd $(dirname ${0}) && pwd)/.env
+cat <<EOT > $(cd $(dirname ${0}) && pwd)/.env
 JUPYTER_DOCKER_IMAGE=${JUPYTER_DOCKER_IMAGE}
 DOCKER_BIN_PATH=$(which docker)
 HOST_USER=$(id -un)
