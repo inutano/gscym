176c274d44a3e306c4fd605d320ae6fce6cad983
Tazro Inutano Ohta <inutano@gmail.com>
2020-07-13 14:43:42 +0900

    create .env from scratch

--- make_env.sh
+++ make_env.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -eu
+
+#
+# Set env variables here
+#
+JUPYTER_DOCKER_IMAGE=jupyter/datascience-notebook:python-3.8.3
+
+rm -f .env
 cat <<EOT >> $(cd $(dirname ${0}) && pwd)/.env
-HOST_UID=`id -u`
-HOST_GID=`id -g`
+JUPYTER_DOCKER_IMAGE=${JUPYTER_DOCKER_IMAGE}
+DOCKER_BIN_PATH=$(which docker)
+HOST_USER=$(id -un)
+HOST_UID=$(id -u)
+HOST_GID=$(id -g)
 EOT
